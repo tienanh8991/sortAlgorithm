7252df453c2439102392405f4bcd97963fcab544
tienanh <tienanh12108991@gmail.com>
2020-04-22 19:08:36 +0700

    up

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+$array = [2, 3, 2, 5, 6, 1, -2, 3, 14, 12];
+
+function bubbleSort($list)
+{
+    for ($i = 0 ; $i < count($list) ; $i++){
+        for ($j = 0 ; $j < count($list) - $i - 1 ; $j++){
+            if ($list[$j] > $list[$j + 1]){
+                $temp = $list[$j];
+                $list[$j] = $list[$j + 1];
+                $list[$j + 1] = $temp;
+            }
+        }
+    }
+    return $list;
+}
+
+var_dump(bubbleSort($array));
